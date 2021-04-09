4b22b198a200ffce8d6d8ae0a442804365c0571f
LucianoRed <lscorsin@redhat.com>
2021-04-08 23:15:18 -0300

    Movido para novo layout

--- index.php
+++ index.php
@@ -1,11 +1,110 @@
-<?php
-error_reporting(0);
-session_start();
-?>
 <html>
 <head>
+<style>
+* {
+        font-family: Arial, "Free Sans";
+    }
+    #container {
+        color: #fff;
+  background:#000;
+    }
+    #container #question {
+        display: block;
+        padding: 20px;
+        font-weight: bold;
+        letter-spacing: -3px;
+        margin-bottom: 20px;
+        padding: 10px;
+        font-size: 40px;
+    }
+    #container div {
+        font-weight: bold;
+        letter-spacing: -3px;
+        background: #0099cc;
+        margin-bottom: 15px;
+        padding: 10px;
+        font-size: 34px;
+        color: #ffffff;
+        border-left: 20px solid #333;
+        width: 400px;
+        -webkit-border-radius: 0.5em;
+        -moz-border-radius: 0 0.5em 0.5em 0;
+        border-radius: 0 1.5em 1.5em 0;
+    }
+    #container div a {
+        border-radius: 0.3em;
+        text-decoration: none;
+        color: #0099cc;
+        padding: 5px 15px;
+        background: #333;
+        margin: 0 20px;
+    }
+    #container div a:hover {
+        color: #fff;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+</head>
+
+<body>
+<?php
+$filename = "/tmp/poll_result.txt";
+$content = file($filename);
+
+//put content in array
+$array = explode("||", $content[0]);
+$yes = $array[0];
+$no = $array[1];
+if($yes == "") {
+$yes = 0;
+}
+if($no == "") {
+$no = 0;
+}
+?>
+<div id="container">
+      <span id="question">What is your favorite server side language?</span>
+      <div id="yes"><span id="yes"><?php echo $yes;?></span><a href="">Vote</a>Sim</div>
+      <div id="no"><span id="no"><?php echo $no;?></span><a href="">Vote</a>Não</div>
+  </div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
-function getVote(int) {
+$(document).ready(function() {
+        // Yes
+        for (let i = 1; i < <?php echo $yes;?>; i++) {
+                setTimeout(function timer() {
+                        $("#yes").animate({width: '+=100px'}, 500);
+                }, i * 100);
+        }
+
+        // No
+        for (let i = 1; i < <?php echo $no;?>; i++) {
+                setTimeout(function timer() {
+                        $("#no").animate({width: '+=100px'}, 500);
+                }, i * 100);
+        }
+
+    $("#container div a").click(function() {
+        $(this).parent().animate({
+           width: '+=100px'
+        }, 500);
+
+        $(this).prev().html(parseInt($(this).prev().html()) + 1);
+
+        //alert($(this).prev().attr('id'));
+        if($(this).prev().attr('id') == "yes") {
+                getVote(0);
+        } else {
+                getVote(1);
+        }
+        return false;
+    });
+
+        function getVote(int) {
   var xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
@@ -15,23 +114,6 @@ function getVote(int) {
   xmlhttp.open("GET","poll_vote.php?vote="+int,true);
   xmlhttp.send();
 }
+});
 </script>
-</head>
-<body>
-<?php
-if(!isset($_SESSION["votou"])) {
-?>
-<div id="poll">
-<h3><?php echo getenv("PERGUNTA");?></h3>
-<form>
-Sim: <input type="radio" name="vote" value="0" onclick="getVote(this.value)"><br>
-Não: <input type="radio" name="vote" value="1" onclick="getVote(this.value)">
-</form>
-</div>
-<?php
-} else {
-echo "Ja votou?<a href=\"poll_vote.php\">Ver resultados</a>";
-}
-?>
-</body>
 </html>
---
+++ index_antigo.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(0);
+session_start();
+?>
+<html>
+<head>
+<script>
+function getVote(int) {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+  xmlhttp.send();
+}
+</script>
+</head>
+<body>
+<?php
+if(!isset($_SESSION["votou"])) {
+?>
+<div id="poll">
+<h3><?php echo getenv("PERGUNTA");?></h3>
+<form>
+Sim: <input type="radio" name="vote" value="0" onclick="getVote(this.value)"><br>
+Não: <input type="radio" name="vote" value="1" onclick="getVote(this.value)">
+</form>
+</div>
+<?php
+} else {
+echo "Ja votou?<a href=\"poll_vote.php\">Ver resultados</a>";
+}
+?>
+</body>
+</html>
